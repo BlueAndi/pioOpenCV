8286e70dd5d3ed80ce8542d6fb8c9f02cd2c75c6
Raphael Sing <Raphael.Sing@newtec.de>
2026-04-10 11:13:34 +0200

    changed structure to match pio library structure

--- copyLibs.bat
+++ copyLibs.bat
@@ -1,17 +1,17 @@
 @echo off
 
 REM Copy all .a files
-xcopy "opencv\build\esp-idf\espressif__opencv\opencv-build\lib\*.a" "opencv_libs\lib\" /Y /I
+xcopy "opencv\build\esp-idf\espressif__opencv\opencv-build\lib\*.a" "lib\" /Y /I
 
 REM Copy all include files from modules
 for /d %%d in (opencv\managed_components\espressif__opencv\opencv\modules\*) do (
     if exist "%%d\include" (
-        xcopy "%%d\include\*" "opencv_libs\include\" /E /Y /I
+        xcopy "%%d\include\*" "include\" /E /Y /I
     )
 )
 
 REM Copy opencv2 directory
-xcopy "opencv\build\esp-idf\espressif__opencv\opencv-build\opencv2" "opencv_libs\include\opencv2\" /E /Y /I
+xcopy "opencv\build\esp-idf\espressif__opencv\opencv-build\opencv2" "include\opencv2\" /E /Y /I
 
 echo Done!
 pause
